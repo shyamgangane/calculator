8b6a93b9edc393c418ae3a3705b95a726681c744
shyamgangane <shyam.ganagne@gmail.com>
2021-04-11 16:45:15 +0530

    addition prog

---
+++ src/com/vit/addition.java
@@ -0,0 +1,14 @@
+package com.vit;
+
+public class addition {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int c = a + b;
+		 
+		System.out.println("Addition for a and b is "  + c);
+
+	}
+
+}
